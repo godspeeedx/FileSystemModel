9d63fb2c23642d68a0fbd211ce9248c72f2e5d51
sasha <sdfddas@yandex.ru>
2021-05-17 00:21:45 +0300

    Create saveSystem test, add saveSys.txt

--- src/main/java/Tests/MethodsForFunctionsTest.java
+++ src/main/java/Tests/MethodsForFunctionsTest.java
@@ -1,5 +1,10 @@
 package Tests;
 
+import Functions.CreateFile;
+import Functions.DownloadSystem;
+import Functions.MethodsForFunctions;
+import Structure.struct.FileSystem;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,5 +14,18 @@ public class MethodsForFunctionsTest {
 
     @Test
     public void saveSystem() {
+
+        System.out.println("Проверка сохранения системы");
+        FileSystem actual = new FileSystem("1",1,1,1);
+        FileSystem expected = new FileSystem("saveSys",10,2,2);
+        CreateFile.createFile(expected,"first",1);
+        CreateFile.createFile(expected,"second",2);
+        CreateFile.createFile(expected,"third",3);
+
+        MethodsForFunctions.saveSystem(expected);
+        DownloadSystem.downloadSystemStatic("saveSys.txt", actual);
+
+        Assert.assertEquals(expected,actual);
+        System.out.println("- Успешно");
     }
 }
