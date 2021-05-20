5ab885377ce701b427a611c4e3b65fa8dad429a9
Tram_ham <57100455+GrigKosilov@users.noreply.github.com>
2021-05-20 13:32:55 +0300

    Чистка

--- src/main/java/Tests/StreamActionsFake.java
+++ src/main/java/Tests/StreamActionsFake.java
@@ -8,23 +8,25 @@ import java.util.Scanner;
 public class StreamActionsFake implements iStreamActions {
 
     ArrayList<String> stringListInput = new ArrayList<String>();
-    ArrayList<String> stringListOutput = new ArrayList<String>();
+    ArrayArrayListList<String> stringListOutput = new ArrayList<String>();
 
     public void println(String value) {
-        stringListOutput.add(value);
+        print(value);
         stringListOutput.add("");
+
     }
+        
 
     public void print(String value) {
-        if      (stringListOutput.size() != 0 &&
-                stringListOutput.get(stringListOutput.size() - 1).isEmpty())
-            stringListOutput.add(value);
-        else
-            stringListOutput.add(stringListOutput.size() - 1, value);
+        if (stringListOutput.isEmpty()) stringListOutput.add("");
+
+        stringListOutput.add(stringListOutput.remove(stringListOutput.size()-1)+value);
+
+       
     }
 
     public String getLine() {
-        if (stringListInput.size()==0)
+        if (stringListInput.isEmpty())
             return "ВЫЙТИ";
         return stringListInput.remove(stringListInput.size() - 1);
     }
